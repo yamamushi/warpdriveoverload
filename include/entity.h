7c4b92e600ca1a173d147d751c16ef4acc68b47c
Fall3nSpork <ian.labelasenoreth@gmail.com>
2014-08-04 01:24:40 -0400

    Trying desperately to remember the formulas for 3-dimensional movement.

--- include/entity.h
+++ include/entity.h
@@ -1,3 +1,19 @@
 class Entity {
-
+	public:
+		entity( std::string type );
+		void updatePos( int newX, int newY, int newZ );
+		void move( int headX, int headY, int headZ, int speed, int duration );
+		
+	protected:
+		int m_xPos;
+		int m_yPos;
+		int m_zPos;
+		float m_height;
+		float m_width;
+		float m_mass;
+		float m_length;
+		int m_xHeading;
+		int m_yHeading;
+		int m_zHeading;
+		std::string type;
 };
