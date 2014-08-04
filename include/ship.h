4074664e7cf6815a230f8c8a9401c83b3f67b574
Fall3nSpork <ian.labelasenoreth@gmail.com>
2014-08-04 07:56:27 -0400

    working more on the ship class;

--- include/ship.h
+++ include/ship.h
@@ -1,17 +1,24 @@
-class Ship {
+class Ship : protected entity {
         public:
-                ship( std::string shipName, std::string fshipClass; ) {
-                }
-		bool move( int impulse, int mass );
+                ship( std::string shipName, std::string fshipClass );
         private:
 		void takeDamage( int dXPos, int dYPos, int dZPos, int dmg );
+		void setClassDefaults( std::string shipClass );
 		int m_scanStrength;
 		int m_sigRadius;
 		int m_vol;
 		void addPart( int slot, int mass );
 		int m_mass;
 		int m_maxPartSlots;
-		std::vector<bool> m_partSlots;
+		int m_maxPartSize;
+		int m_shipSizeClass;
+		std::vector<int> m_thrusterSlots;
+		std::vector<int> m_reactorSlots;
+		std::vector<int> m_weaponSlots;
+		std::vector<int> m_shieldSlots;
+		std::vector<int> m_sensorSlots;
+		std::vector<int> m_commSlots;
+		std::vector<int> m_armorSlots;
                 int m_shieldHP;
                 int m_maxShield;
                 float m_armorMod;
@@ -23,9 +30,6 @@ class Ship {
 		int m_pGridMax;
                 std::string m_shipClass;
                 std::string m_name;
-		int m_xPos;
-		int m_yPos;
-		int m_zPos;
 		int m_thrust;
 		int m_maxThrust;
 		bool m_accelerate();
